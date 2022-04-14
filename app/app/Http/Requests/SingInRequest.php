cf504b0425e36ecbd01adcfc9eb238e277157dba
PDA2304 <isip_d.a.pahomov@mpt.ru>
2022-04-14 10:22:56 -0700

    Исправлено унаследование.

--- app/app/Http/Requests/SingInRequest.php
+++ app/app/Http/Requests/SingInRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
 
-class SingInRequest extends FormRequest
+class SingInRequest extends ApiRequest
 {
     
     public function rules()
