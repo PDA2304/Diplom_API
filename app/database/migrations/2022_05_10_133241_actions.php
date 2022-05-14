55e4a9aaf1abb9e51f953e7a3dbfc32ecb5b7c20
PDA2304 <isip_d.a.pahomov@mpt.ru>
2022-05-13 21:59:51 -0700

    Были добавлены 3 таблицы: - Тип таблицы - тип действия - Действия Так же был реализовал seeder для создание триггеров и начальных данных в базе данных.

---
+++ app/database/migrations/2022_05_10_133241_actions.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actions', function (Blueprint $table) {
+            $table->id();
+            $table->date('action_date');
+            $table->integer('user_id');
+            $table->integer('data_id');
+            $table->integer('type_table_id');
+            $table->integer('type_action_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('type_action_id')->references('id')->on('type_actions')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('type_table_id')->references('id')->on('type_table')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actions');
+    }
+};
--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\TypaTable;
+use App\Models\TypeAction;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +19,150 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $typeActionName = array(
+            'Добавление',
+            'Данные были перемещены в корзину',
+            'Данные были восстановлены',
+            'Данные были изменены',
+            'Доступ к данным был увеличен',
+            'Доступ к данным был уменьшен',
+        );
+
+        $typeTable = array(
+            'notes',
+            'files',
+            'account',
+            'data',
+        );
+
+        foreach ($typeTable as $key) {
+            TypaTable::create(
+                [
+                    'table_name' => $key
+                ]
+            );
+        }
+
+        foreach ($typeActionName as $key) {
+            TypeAction::create(
+                [
+                    'action_name' => $key
+                ]
+            );
+        }
+
+        $employee = new User();
+
+        $employee->create([
+            'role' => true,
+            'user_name' => 'administrator',
+            'login' => 'admin',
+            'password' => Hash::make('admin'),
+        ]);
+
+
+        DB::statement('
+            CREATE OR REPLACE FUNCTION public.history_account()
+            RETURNS trigger
+            LANGUAGE plpgsql
+            COST 100
+            VOLATILE NOT LEAKPROOF
+            AS $BODY$
+                begin
+                    if(TG_OP = \'INSERT\') then
+                    INSERT INTO public.actions(
+                        action_date, user_id, type_action_id,data_id,type_table_id)
+                        VALUES (clock_timestamp(), NEW.user_id, 1,NEW.id,3);
+                        return NEW;
+                    end if;
+                    if(TG_OP = \'UPDATE\') then
+                        if(NEW.account_name != OLD.account_name OR NEW.login != OLD.login OR NEW.password != OLD.password OR NEW.description != OLD.description)
+                        then 
+                            INSERT INTO public.actions(
+                            action_date, user_id, type_action_id,data_id,type_table_id)
+                            VALUES (clock_timestamp(), OLD.user_id, 4,OLD.id,3);
+                        return NEW;
+                        end if;
+                            
+                        if(NEW.logic_delete = false) 
+                        then 
+                            INSERT INTO public.actions(
+                            action_date, user_id, type_action_id,data_id,type_table_id)
+                            VALUES (clock_timestamp(), OLD.user_id, 3,OLD.id,3);
+                        end if;
+                        
+                        if(NEW.logic_delete = true) 
+                        then 
+                            INSERT INTO public.actions(
+                            action_date, user_id, type_action_id,data_id,type_table_id)
+                            VALUES (clock_timestamp(), OLD.user_id, 2,OLD.id,3);
+                        end if;
+                        
+                        RETURN NULL;
+                    end if;
+                    
+                end;
+                $BODY$;
+        ');
+
+        DB::statement('  
+            CREATE TRIGGER history_account
+            AFTER INSERT OR UPDATE 
+            ON public.account
+            FOR EACH ROW
+            EXECUTE FUNCTION public.history_account();
+        ');
+        
+        DB::statement('
+            CREATE OR REPLACE FUNCTION public.history_notes()
+            RETURNS trigger
+            LANGUAGE \'plpgsql\'
+            COST 100
+            VOLATILE NOT LEAKPROOF
+            AS $BODY$
+                    begin
+                        if(TG_OP = \'INSERT\') then
+                        INSERT INTO public.actions(
+                            action_date, user_id, type_action_id,data_id,type_table_id)
+                            VALUES (clock_timestamp(), NEW.user_id, 1,NEW.id,1);
+                            return NEW;
+                        end if;
+                        if(TG_OP = \'UPDATE\') then
+                            if(NEW.notes_name != OLD.notes_name OR NEW.content != OLD.content OR NEW.description != OLD.description)
+                            then 
+                                INSERT INTO public.actions(
+                                action_date, user_id, type_action_id,data_id,type_table_id)
+                                VALUES (clock_timestamp(), OLD.user_id, 4,OLD.id,1);
+                            return NEW;
+                            end if;
+                                
+                            if(NEW.logic_delete = false) 
+                            then 
+                                INSERT INTO public.actions(
+                                action_date, user_id, type_action_id,data_id,type_table_id)
+                                VALUES (clock_timestamp(), OLD.user_id, 3,OLD.id,1);
+                            end if;
+                            
+                            if(NEW.logic_delete = true) 
+                            then 
+                                INSERT INTO public.actions(
+                                action_date, user_id, type_action_id,data_id,type_table_id)
+                                VALUES (clock_timestamp(), OLD.user_id, 2,OLD.id,1);
+                            end if;
+                            
+                            RETURN NULL;
+                        end if;
+                        
+                    end;
+                    $BODY$;
+        ');
+
+        DB::statement('  
+            CREATE TRIGGER history_notes
+            AFTER INSERT OR UPDATE 
+            ON public.notes
+            FOR EACH ROW
+            EXECUTE FUNCTION public.history_notes();
+        ');
     }
 }
